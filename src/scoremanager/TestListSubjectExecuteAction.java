4f51c5777e7601fc345077982886e2708688a5d0
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-05-08 10:35:26 +0900

    Conflict

--- src/scoremanager/TestListSubjectExecuteAction.java
+++ src/scoremanager/TestListSubjectExecuteAction.java
@@ -8,10 +8,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Subject;
 import bean.Test;
 
 public class TestListSubjectExecuteAction {
-    public List<Test> execute(String subject) {
+	public void execute(HttpServletRequest req, HttpServletResponse res) 
+			throws Exception {// ユーザーデータを取得＆セレクトボックス用のクラスデータを取得	
+
+    	Subject subject = new Subject();
         List<Test> testList = new ArrayList<>();
         // データベース接続情報
         String url = "jdbc:h2:tcp://localhost/~/exam";
@@ -21,12 +28,12 @@ public class TestListSubjectExecuteAction {
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             String query = "SELECT * FROM test_scores WHERE subject = ?";
             try (PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setString(1, subject);
+                statement.setString(1, subject.getName());
                 try (ResultSet resultSet = statement.executeQuery()) {
                     while (resultSet.next()) {
                         String studentId = resultSet.getString("student_id");
                         int score = resultSet.getInt("score");
-                        Test test = new Test(studentId, subject, score);
+                        Test test = new Test(studentId, subject.getName(), score);
                         testList.add(test);
                     }
                 }
@@ -35,7 +42,9 @@ public class TestListSubjectExecuteAction {
             e.printStackTrace();
             // エラーハンドリングを行う
         }
-        return testList;
+//        return testList;
+        req.setAttribute("subject", subject);
+        req.getRequestDispatcher("test_list_student.jsp").forward(req, res);
     }
 }
 
