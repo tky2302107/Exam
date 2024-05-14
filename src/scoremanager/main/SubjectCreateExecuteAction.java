0e9d3f445da8c54624ba60f86dd3a8536c9b1331
nao-0802 <tky2302123@stu.o-hara.ac.jp>
2024-05-14 11:18:41 +0900

    サーブレットの追加

---
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -0,0 +1,63 @@
+package scoremanager.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectCreateExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		String cd = "";
+		String name = "";
+		SubjectDao sDao = new SubjectDao();
+		Map<String, String> errors = new HashMap<>();
+
+		cd = req.getParameter("cd");
+		name = req.getParameter("name");
+
+		if (cd.length() != 3){
+			errors.put("f1", "科目コードは3文字で入力してください");
+			req.setAttribute("errors", errors);
+			req.setAttribute("cd", cd);
+			req.setAttribute("name", name);
+			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+			return;
+		}else{
+		try{
+			Subject subject = new Subject();
+			subject.setCd(cd);
+			subject.setName(name);
+			subject.setSchool(teacher.getSchool());
+			Subject subject2 = new Subject();
+			subject2 = sDao.get(cd, teacher.getSchool());
+			if(subject2 != null){
+				errors.put("f2", "科目コードが重複しています。");
+				req.setAttribute("errors", errors);
+				req.setAttribute("cd", cd);
+				req.setAttribute("name", name);
+				req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+				return;
+			}
+
+			sDao.save(subject);
+
+		}catch (Exception e) {
+			throw e;
+		}
+		req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
+		}
+	}
+}
---
+++ src/scoremanager/main/TestRegistExecuteAction.java
@@ -0,0 +1,60 @@
+package scoremanager.main;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Student;
+import bean.Subject;
+import bean.Teacher;
+import bean.Test;
+import dao.StudentDao;
+import dao.SubjectDao;
+import dao.TestDao;
+import tool.Action;
+import tool.Util;
+
+public class TestRegistExecuteAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		Util util = new Util();
+		Teacher teacher = util.getUser(req);
+		Enumeration<String> paramlist = req.getParameterNames();
+		List<Test> list = new ArrayList<>();
+		int no = Integer.parseInt(req.getParameter("f4"));
+		TestDao testDao = new TestDao();
+		String cd = req.getParameter("f3");
+		Subject subject = new Subject();
+		SubjectDao subjectDao = new SubjectDao();
+		StudentDao studentDao = new StudentDao();
+		subject = subjectDao.get(cd, teacher.getSchool());
+		String classNum = req.getParameter("f2");
+		while(paramlist.hasMoreElements()){
+			String name = paramlist.nextElement();
+			if(name.startsWith("point_")){
+				String student_cd = name.substring(6);
+				try{
+					Test test = new Test();
+					Student student = new Student();
+					student = studentDao.get(student_cd);
+					test.setStudent(student);
+					test.setSubject(subject);
+					test.setSchool(teacher.getSchool());
+					test.setNo(no);
+					test.setPoint(Integer.parseInt(req.getParameter("point_"+student_cd)));
+					test.setClassNum(classNum);
+					list.add(test);
+				}catch(Exception e){
+					throw e;
+				}
+			}
+		}
+		testDao.save(list);
+		req.getRequestDispatcher("test_regist_done.jsp").forward(req, res);
+	}
+
+}
